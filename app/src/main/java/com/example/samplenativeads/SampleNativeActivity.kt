4fe01c4066c5ed99349eef81e51898fd2c3b2cfc
GiangPT <GiangPT@apero.vn>
2024-09-23 18:26:00 +0700

    init

--- app/src/main/java/com/example/samplenativeads/SampleNativeActivity.kt
+++ app/src/main/java/com/example/samplenativeads/SampleNativeActivity.kt
@@ -5,18 +5,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.samplenativeads.databinding.ActivitySampleNativeBinding
 
 class SampleNativeActivity : AppCompatActivity() {
 
-
+    private lateinit var binding: ActivitySampleNativeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_sample_native)
+        binding = ActivitySampleNativeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
     }
 }
